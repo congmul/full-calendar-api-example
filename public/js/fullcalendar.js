d05ea95fe7febd2fbd1b742b62bae7cfd9dc7de4
Jehyun <congmul@hotmail.com>
2022-06-03 09:16:36 -0700

    Update Calendar

--- public/js/fullcalendar.js
+++ public/js/fullcalendar.js
@@ -1,9 +1,39 @@
 document.addEventListener('DOMContentLoaded', function() {
     const calendarEl = document.getElementById('calendar');
     const calendar = new FullCalendar.Calendar(calendarEl, {
-        // initialView: 'dayGridMonth'
-        // initialView: 'timeGridWeek'
-        initialView: 'timeGridDay'
+        initialView: 'dayGridMonth',
+        headerToolbar: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+          },
+        timeZone: 'UTC',
+        editable: true,
+        dayMaxEvents: true, // when too many events in a day, show the popover
+        dateClick: function(info) {
+            var event = prompt('Enter Your Event');
+            if(event != null && event != ""){
+                calendar.addEvent({
+                    title: event,
+                    start: info.dateStr,
+                    allDay: true
+                });
+            }
+            //TODO: Call Database to store this event
+        },
+        // TODO: Get Events a specific user has
+        events: [
+            {
+                id: 'a',
+                title: 'my event',
+                start: '2022-06-03 15:00:00',
+                end: '2022-06-04 10:00:00'
+            }
+        ]
     });
     calendar.render();
+    
+    // var event = calendar.getEventById('a')
+    // event.remove()
 });
+
